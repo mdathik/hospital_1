cd27bed0aee25ae99e4dd7c504350cba9f9097ca
mdathik <mdathik2004@gmail.com>
2025-02-24 16:59:46 +0530

    Update DBConnect.java

--- src/main/java/com/db/DBConnect.java
+++ src/main/java/com/db/DBConnect.java
@@ -4,19 +4,23 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnect {
+    private static Connection conn;
 
-	private static Connection conn;
+    public static Connection getConn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Load MySQL Driver
 
-	public static Connection getConn() {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital_2", "root", "7860");
+            // Replace with Railway Database Details
+            String jdbcUrl = "jdbc:mysql://mysql.railway.internal.railway.app:3306/railway?useSSL=false&allowPublicKeyRetrieval=true";
+            String dbUser = "root";
+            String dbPass = "UvwGBUCZkCywMkpbsWtfBRPQNegfGRbT";
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+            conn = DriverManager.getConnection(jdbcUrl, dbUser, dbPass);
 
-		return conn;
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        return conn;
+    }
 }
